27ddc4597d9f2a6f1d860523a094eb27017c2ed6
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-06-25 18:04:14 +0200

    state when the component mounts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import './App.css';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 import { Route, Switch } from 'react-router-dom';
-import countries from './countries.json'
+import countriesJSON from './countries.json'
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
+class App extends Component {
+  state = {
+    countries: [],
+  }
 
-      <div className="container flex mx-auto my-8 ">
-        <CountriesList countriesList={countries}/>
-        <Switch>
-          <Route exact path="/:cca3"  
-            render={ (routerProps) => 
-            <CountryDetails {...routerProps} countriesList={countries} />
-            } 
-          />
-        </Switch>
+  componentDidMount() {
+    this.setState({
+      countries: countriesJSON
+    })
+  }
+
+
+  render() {
+    const { countries } = this.state;
+
+    return (
+      <div className="App">
+        <Navbar />
+  
+        <div className="container flex mx-auto my-8 ">
+          <CountriesList countriesList={countries}/>
+          <Switch>
+            <Route exact path="/:cca3"  
+              render={ (routerProps) => 
+              <CountryDetails {...routerProps} countriesList={countries} />
+              } 
+            />
+          </Switch>
+        </div>
       </div>
-        
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -10,7 +10,7 @@ export class CountriesList extends Component {
             <h2  className="text-left font-black text-xl mb-4">Countries List:</h2>
             {countriesList.map((country, index) => {
               return (
-                <div key={index} className="container border mb-4 py-4">
+                <div key={index} className="container border mb-4 py-4 ml-4">
                   <Link to={`/${country.cca3}`} key={country.cca3}>
                     <span role='img'>{country.flag}</span>
                     {country.name.common}
